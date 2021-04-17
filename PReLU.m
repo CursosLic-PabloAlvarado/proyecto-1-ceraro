626f0b418cb29734d04479d147162d4d3a2e7813
Olivier <olivierjdta.2@hotmail.com>
2021-04-17 14:38:45 -0600

    Propiedades de clase definidas

--- PReLU.m
+++ PReLU.m
@@ -1,4 +1,10 @@
 ## "Capa" PReLU, 
 classdef PReLU < handle
+  properties    
+    ## Resultados después de la propagación hacia adelante
+    outputs=[];
+    ## Resultados después de la propagación hacia atrás
+    gradient=[];
+  endproperties
   
 endclassdef
