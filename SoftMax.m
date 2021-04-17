803022f39b3d06cdc31b0916b925578265afab75
Olivier <olivierjdta.2@hotmail.com>
2021-04-17 14:54:47 -0600

    Metodos agregados

--- SoftMax.m
+++ SoftMax.m
@@ -6,4 +6,28 @@ classdef ReLU < handle
     ## Resultados después de la propagación hacia atrás
     gradient=[];
   endproperties
+  
+  methods
+    ## Constructor ejecuta un forward si se le pasan datos
+    function s=SoftMax()
+      s.outputs=[];
+      s.gradient=[];
+    endfunction
+
+    ## En funciones de activación el init no hace mayor cosa más que
+    ## indicar que la dimensión de la salida es la misma que la entrada.
+    ##
+    ## La función devuelve la dimensión de la salida de la capa
+    function outSize=init(s,inputSize)
+      outSize=inputSize;
+    endfunction 
+
+    ## Retorna false si la capa no tiene un estado que adaptar
+    function st=hasState(s)
+      st=false;
+    endfunction
+
+
+  endmethods
+  
 endclassdef
