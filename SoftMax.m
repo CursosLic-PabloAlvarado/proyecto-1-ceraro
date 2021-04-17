a152b7f3cd8384820c719ac811036632be808e81
Olivier <olivierjdta.2@hotmail.com>
2021-04-17 14:53:28 -0600

    Propiedades de clase definidas

--- SoftMax.m
+++ SoftMax.m
@@ -1,4 +1,9 @@
 ## "Capa" ReLU, 
 classdef ReLU < handle
-
+  properties    
+    ## Resultados después de la propagación hacia adelante
+    outputs=[];
+    ## Resultados después de la propagación hacia atrás
+    gradient=[];
+  endproperties
 endclassdef
