cbf359a155d3622d0e8dda84f9df471c7c4d4f84
RDCerdas <72822361+RDCerdas@users.noreply.github.com>
2021-04-29 14:41:31 -0600

    cambios normalizacion

--- train.m
+++ train.m
@@ -41,13 +41,13 @@ else # Aqui estoy formando una red
   
   # Se está usando un cell arrays (arreglo de celdas) {}. Una celda es cualquier cosa
   ann.add({input_layer(2), # Capa de entrada que recibe 2 dimensiones
-           batchnorm(),
+           #batchnorm(),
            dense_unbiased(16), # Capa densa sin sesgo 
-           sigmoide(), #
-           batchnorm(), 
+           ReLU(), #
+           #batchnorm(), 
            dense_unbiased(16),
-           sigmoide(),
-           batchnorm(),
+           ReLU(),
+           #batchnorm(),
            dense_unbiased(numClasses),
            SoftMax()});
   ann.add(MSE()); # Capa de pérdida
