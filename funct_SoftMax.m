1fa54ceb58ccb1ab1b8511cebd52303c6af82a44
Olivier <olivierjdta.2@hotmail.com>
2021-05-01 09:53:03 -0600

    Clase k agregada

--- funct_SoftMax.m
+++ funct_SoftMax.m
@@ -4,7 +4,7 @@ function val=funct_SoftMax(x)
   else
     val = exp(x-max(x,[],2)); # Es para una matriz de diseño
                               # Octave aplica un broadcasting por eso no es necesario multiplicaciones por vectores de 1's
-    nor = sum(val);
+    nor = sum(val)+ones(1,columns(val));
     val = val ./ nor;
   endif
 endfunction
